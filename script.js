4f0f4843ebb1f8064c41c0440acf7e3edf6f2c49
Saidazim2011 <oergasev75@gmail.com>
2025-04-25 21:51:28 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const lightModeBtn = document.getElementById('lightModeBtn');
+const darkModeBtn = document.getElementById('darkModeBtn');
+
+lightModeBtn.addEventListener('click', () => {
+  document.body.classList.remove('dark-mode');
+});
+
+darkModeBtn.addEventListener('click', () => {
+  document.body.classList.add('dark-mode');
+});
